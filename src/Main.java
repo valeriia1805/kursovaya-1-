b99946c9e99090789ce80b31be10da9a2656c086
valeriia1805 <130702708+valeriia1805@users.noreply.github.com>
2025-08-29 18:56:06 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,69 +1,5 @@
 import java.util.Objects;
-
-class Employee {
-    private static int idCounter = 1;
-
-    private final int id;
-    private final String fullName;
-    private int department;
-    private double salary;
-
-    public Employee(String fullName, int department, double salary) {
-        this.id = idCounter++;
-        this.fullName = fullName;
-        this.department = department;
-        this.salary = salary;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public int getDepartment() {
-        return department;
-    }
-
-    public double getSalary() {
-        return salary;
-    }
-
-    public void setDepartment(int department) {
-        this.department = department;
-    }
-
-    public void setSalary(double salary) {
-        this.salary = salary;
-    }
-
-    @Override
-    public String toString() {
-        return "Employee{" +
-               "id=" + id +
-               ", fullName='" + fullName + '\'' +
-               ", department=" + department +
-               ", salary=" + salary +
-               '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Employee employee)) return false;
-        return id == employee.id &&
-               department == employee.department &&
-               Double.compare(employee.salary, salary) == 0 &&
-               fullName.equals(employee.fullName);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, fullName, department, salary);
-    }
-}
+import Employee;
 
 public class Main {
     private static final Employee[] employees = new Employee[10];
@@ -133,3 +69,4 @@ public class Main {
         }
     }
 }
+
